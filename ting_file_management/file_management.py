7158950f8062ae0a5aeb0cdb18ef4772508bad1e
Danilo Martins <dansm86@outlook.com>
2023-04-06 19:10:20 -0300

    feat:req02/implemented txt_importer fn

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    file_extension = str(path_file).split(".")[-1]
+    if file_extension != 'txt':
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, encoding="utf-8") as txt_file:
+            text = [
+                text.strip() for text in txt_file
+            ]
+            return text
+    except (IsADirectoryError, FileNotFoundError):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
