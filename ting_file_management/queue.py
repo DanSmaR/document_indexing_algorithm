b72c47653f8eb33e6e97faef69c8a38dd83eb56e
Danilo Martins <dansm86@outlook.com>
2023-04-06 17:46:55 -0300

    feat:req01/implemented queue data structure

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,31 @@
+from __future__ import annotations
+from typing import Generic, TypeVar
+
+
 from ting_file_management.abstract_queue import AbstractQueue
+from utils.linked_list import LinkedList
 
 
-class Queue(AbstractQueue):
-    def __init__(self):
-        """Inicialize sua estrutura aqui"""
+T = TypeVar("T")
 
-    def __len__(self):
-        """Aqui irá sua implementação"""
 
-    def enqueue(self, value):
-        """Aqui irá sua implementação"""
+class Queue(AbstractQueue, Generic[T]):
+    def __init__(self) -> None:
+        self.__list: LinkedList[T] = LinkedList()
+
+    def __len__(self) -> int:
+        return len(self.__list)
+
+    def is_empty(self) -> bool:
+        return self.__list.is_empty()
+
+    def enqueue(self, value: T) -> None:
+        self.__list.add_last(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            raise RuntimeError("Queue is already empty")
+        return self.__list.remove_first()
 
-    def search(self, index):
-        """Aqui irá sua implementação"""
+    def search(self, index: int) -> T:
+        return self.__list.get_element_at(index)
